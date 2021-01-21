a7d49ddd9f0c0f7cc6b910f6dae99400c77f43bd
edeani <anloder4@hotmail.com>
2021-01-20 21:55:39 -0500

    tests- tests and coverage with mocha and nyc

---
+++ test/app-test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const expect= require('chai').expect;
+const handler = require('../handler');
+const fs = require('fs');
+const path = require('path');
+
+let rawdata = fs.readFileSync(path.resolve('test/test-environment.json'));
+let student = JSON.parse(rawdata);
+
+
+global.config_  =  {
+    connectionDatabase:student
+};
+
+describe('Is mutant', function(){
+    it('Test is mutant', async function(){
+        let event = {
+            body:'{'+
+                '"dna":["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"]'
+            +'}'
+        };
+
+        expect((await handler.mutant(event)).statusCode).to.equal(200);
+    });
+
+    
+});
+
+describe('Is not mutant', function(){
+    it('Test is not mutant', async function(){
+        let event = {
+            body:'{'+
+                '"dna":["TTGCGT","CAGTAC","TTATGT","AGAAGG","GCCCTA","TCACTG"]'
+            +'}'
+        };
+
+        expect((await handler.mutant(event)).statusCode).to.equal(403);
+    });
+
+    
+});
+
+describe('stats--test', function(){
+    it('Test stats', async function(){
+        let event = {          
+        };
+
+        expect(JSON.parse((await handler.stats(event)).body).count_human_dna).gte(0);
+
+    });
+
+    
+});
