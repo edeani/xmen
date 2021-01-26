c098fb412ec541d5fe320a7358c4ed1713bb3d8a
edeani <anloder4@hotmail.com>
2021-01-26 16:15:01 -0500

    main - Verify valid proteins 2

--- handler.js
+++ handler.js
@@ -11,7 +11,7 @@ module.exports.mutant = async (event) => {
   let dna = requestBodyMutant.dna;
 
   let isMutantArr = await personService.mutant(dna);
-  if(isMutantArr != undefined || isMutantArr!=null){
+  if((isMutantArr != undefined || isMutantArr!=null) && isMutantArr[0]!=2){
     await personService.save(dna,isMutantArr[1]);
   }
   if(isMutantArr[1]){
@@ -25,14 +25,26 @@ module.exports.mutant = async (event) => {
       )
     }
   }else{
-    return {
-      statusCode: 403,
-      body: JSON.stringify(
-        {
-          message: 'Forbidden'
-        }
-      ),
-  }
+    if(isMutantArr[0]==2){
+      return {
+        statusCode: 403,
+        body: JSON.stringify(
+          {
+            message: 'Forbidden: Invalid characters'
+          }
+        ),
+      }
+    }else{
+      return {
+        statusCode: 403,
+        body: JSON.stringify(
+          {
+            message: 'Forbidden'
+          }
+        ),
+      }
+    }
+    
  
   };
 
